5f4e46af69a02cece5181f91ae66ea764e0e3ca0
tj4001 <71589105+tj4001@users.noreply.github.com>
2020-11-06 15:21:53 +0530

    Update container-restart.sh

--- code-docker/container-restart.sh
+++ code-docker/container-restart.sh
@@ -1,1 +1,8 @@
+data= docker ps -a | awk '{if (NR!=1) {print$1}}'
+#data=$(docker ps -a)
+# print $data.[2]
 
+for item in $data:
+    do echo $item
+
+#docker ps -a  $(awk '{if (NR!=1) {print$1}}')
